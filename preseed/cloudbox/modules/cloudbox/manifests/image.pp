d8e05061b6c826edebf93dd74d8c624816c8973d
acidprime <zack.smith@puppetlabs.com>
2012-10-03 17:09:29 -0700

    Added glance image type declaration

--- preseed/cloudbox/modules/cloudbox/manifests/image.pp
+++ preseed/cloudbox/modules/cloudbox/manifests/image.pp
@@ -1,12 +1,20 @@
 class cloudbox::image {
-  exec { 'add_image':
-    path    => '/usr/bin',
-    command => 'glance \
-      add -I admin \
-      -K puppet \
-      name=centos-5.7-pe-2.5.2  \
-      is_public=true \
-      container_format=bare \
-      disk_format=qcow2 <centos-5.7-pe-2.5.2.img',
+  glance_image { 'training_image':
+    ensure           => present,
+    name             => "centos-5.7-pe-2.5.2",
+    is_public        => true,
+    container_format => bare,
+    disk_format      => qcow2,
+    source           => '/cloudbox/preseed/cloudbox/centos-5.7-pe-2.5.2.img',
   }
+  #exec { 'add_image':
+  #  path    => '/usr/bin',
+  #  command => 'glance \
+  #    add -I admin \
+  #    -K puppet \
+  #    name=centos-5.7-pe-2.5.2  \
+  #    is_public=true \
+  #    container_format=bare \
+  #    disk_format=qcow2 <centos-5.7-pe-2.5.2.img',
+  #}
 } 
